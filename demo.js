ccd1c377abb8c07172fbfd0574124879437672e9
Johan Veenpere <johan@SYSTEM1.lan>
2020-11-30 23:33:47 +0200

    valideerija errorite parandamine ja koodi kommenteerimine

--- demo.js
+++ demo.js
@@ -1,15 +1,26 @@
+//Autor: Johan Veenpere
+
+//kuna pythoni kood on keeruline veebilehel tööle panna, siis on projekt porditud javascripti
+
 function displayTime(){
+    //praegune aeg
     let date = new Date;
-    let minutestring    = date.getMinutes().toString(2);
-    let hourstring      = date.getHours().toString(2);
-    let outstring = (hourstring + " " + minutestring).replace(/1/g, "🐜").replace(/0/g, "🕷️");
+    //tekitatakse minutite ja tundide stringid
+    let minutestring    = ("000000" + date.getMinutes().toString(2)).slice(-6);
+    let hourstring      = ("000000" + date.getHours().toString(2)).slice(-6);
+    //asendatakse 0 ja 1 ämblike ja sipelgatega
+    let outstring = (hourstring + ":" + minutestring).replace(/1/g, "🐜").replace(/0/g, "🕷️");
     console.log(outstring);
+    //näidatakse aega lehel
     document.getElementById("stickyheader").innerText = outstring;
 }
+
 function main(){
     console.log("in main");
     displayTime();
+    //seatakse funktsiooni väljakutsumiste intervall
     setInterval(displayTime, 60000);
 }
 
+//kuna kasutatakse html elementi, siis tuleb enne js käivitamist oodata, et leht ära laeks
 document.addEventListener("DOMContentLoaded", main);
--- stickyheader.js
+++ stickyheader.js
@@ -1,13 +1,13 @@
-let element;
-let elementRect;
-let distanceFromTop;
+//Autor: Johan Veenpere
+//read 8 ja 9 võetud aadressilt https://stackoverflow.com/questions/16302483/event-to-detect-when-positionsticky-is-triggered
 
 function main(){
+    let element;
     element = document.getElementById("stickyheader");
-
+    //jälgitakse, kas stickyheader element läheb vastu ekraani ülemist äärt. Kui läheb, siis pannakse külge klass stickied
     const observer = new IntersectionObserver( ([e]) => e.target.classList.toggle("stickied", e.intersectionRatio < 1), {threshold: [1]});
     observer.observe(element); 
 }
 
+//kuna kasutatakse html elementi, siis tuleb enne js käivitamist oodata, et leht ära laeks
 document.addEventListener("DOMContentLoaded", main);
-//document.addEventListener("scroll", onScroll);
